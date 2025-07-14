d3e4668d874ca1eece778b75a7eee02052bc13c0
Shubhansh Sharma <shubhanshsharma030604@gmail.com>
2025-07-14 16:51:01 +0530

    index files in v1/v2

--- ROUTES/index.js
+++ ROUTES/index.js
@@ -1,15 +1,11 @@
-
-
 const express = require("express");
 const router = express.Router();
 
-const authRoutes = require("./v1/auth.routes");
-const blogRoutes = require("./v1/blog.routes");
-const blogRoutesV2 = require("./v2//blog.routes");
+const v1Routes = require("./v1");
+const v2Routes = require("./v2");
 
-// Mount with versioning prefix
-router.use("/api/v1/auth", authRoutes);
-router.use("/api/v1/blog", blogRoutes);
-router.use("/api/v2/blog", blogRoutesV2);
+// Mount with version prefixes
+router.use("/api/v1", v1Routes);
+router.use("/api/v2", v2Routes);
 
 module.exports = router;
--- ROUTES/v2/index.js
+++ ROUTES/v2/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const authRoutes = require("./auth.routes");
 const blogRoutes = require("./blog.routes");
 
-router.use("/auth", authRoutes);
 router.use("/blog", blogRoutes);
 
 module.exports = router;
